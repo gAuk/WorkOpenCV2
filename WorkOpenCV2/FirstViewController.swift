99f8715bd82a8a62ccdf141f47680d3ff3b5de42
YamanaToshiyuki <yamanatoshiyuki@YamanaMacAir.local>
2014-12-21 13:49:06 +0900

    Initial Commit

---
+++ WorkOpenCV2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WorkOpenCV2
+//
+//  Created by YamanaToshiyuki on 2014/12/21.
+//  Copyright (c) 2014年 YamanaToshiyuki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WorkOpenCV2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WorkOpenCV2
+//
+//  Created by YamanaToshiyuki on 2014/12/21.
+//  Copyright (c) 2014年 YamanaToshiyuki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
