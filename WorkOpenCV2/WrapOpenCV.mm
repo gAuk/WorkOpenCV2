4336f520cf1646f590f7a20f8a006bf6b3c434a1
YamanaToshiyuki <tyamana3@gmail.com>
2014-12-23 13:31:22 +0900

    Makefileとtravis.ymlを追加

---
+++ WorkOpenCV2/WrapOpenCV.mm
@@ -0,0 +1,9 @@
+//
+//  WrapOpenCV.m
+//  WorkOpenCV2
+//
+//  Created by YamanaToshiyuki on 2014/12/21.
+//  Copyright (c) 2014年 YamanaToshiyuki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
